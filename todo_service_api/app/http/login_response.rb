6188af6941d98c22fc7bfc3c77e5026a3e2bbf9a
locvt1 <locvt1@hdbank.com.vn>
2025-04-14 17:19:23 +0700

    fix my dumb error

--- todo_service_api/app/http/login_response.rb
+++ todo_service_api/app/http/login_response.rb
@@ -5,4 +5,21 @@ class LoginResponse
     @user_uid = user_uid
     @token = token
   end
+
+  def username
+    @username
+  end
+
+  def full_name
+    @full_name
+  end
+
+  def user_uid
+    @user_uid
+  end
+
+  def token
+    @token
+  end
+
 end
